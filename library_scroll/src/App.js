d45c7f6a67e9d9822a6b9334010ce8f6d1f6343d
Michael Littleton <michaelglittleton@gmail.com>
2021-02-20 17:05:43 -0500

    Displaying pics

--- library_scroll/src/App.js
+++ library_scroll/src/App.js
@@ -6,6 +6,8 @@ import Title from './Components/Title';
 import Scroll from './Components/Scroll';
 import Description from './Components/Description';
 
+const googleBooksAPI = 'AIzaSyCTE-A9T51L4HTR00zPIyAY5SpvLnN0tOg';
+
 class App extends Component {
   constructor() {
     super();
@@ -13,18 +15,21 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://openlibrary.org/works/OL45883W.json')
-      .then((res) => res.json())
-      .then((result) => {
-        const pics = result.covers.filter(e => {
-          return e > 0
-        })
-        this.setState({ pics: pics });
-      });
+    devBooks.forEach(e => {
+      fetch(`https://www.googleapis.com/books/v1/volumes?q=${e}&key=${googleBooksAPI}`)
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result)
+          const pics = result.totalItems ? result.items[0].volumeInfo : false
+          const newPics = this.state.pics
+          newPics.push(pics)
+          this.setState({ pics: newPics });
+       });
+    })
   }
 
   render() {
-    console.log(this.state.pics)
+    console.log(this.state.pics);
     return (
       <div className="App">
         <FlexColumn
@@ -43,3 +48,20 @@ class App extends Component {
 }
 
 export default App;
+
+const devBooks = [
+  9781603090254,
+  9781603090476,
+  9781891830853,
+  9781603090162,
+  9781603092654,
+  9781603090773,
+  9781603093699,
+  9781603090261,
+  9781603090278,
+  9781603094054,
+  9781603094429,
+  9781603094788,
+  9781603092739,
+  9781603094320,
+];
--- library_scroll/src/Components/Scroll.js
+++ library_scroll/src/Components/Scroll.js
@@ -3,9 +3,16 @@ import { FlexRow } from './customComponents';
 
 function Scroll(props) {
   return (
-    <FlexRow alignCenter flex='3' >
-      <img src={`https://covers.openlibrary.org/b/id/${props.pics[0]}-M.jpg`}/>
-      <img src={`https://covers.openlibrary.org/b/id/${props.pics[1]}-M.jpg`}/>
+    <FlexRow alignCenter flex="3">
+      {props.pics.map((e, i) => {
+        if (!e || !e.imageLinks) {
+          return (
+            <img key={i} src="https://books.google.com/googlebooks/images/no_cover_thumb.gif" />
+          );
+        }
+        console.log("check here for a thumbnail", e)
+        return <img src={e.imageLinks.thumbnail} />;
+      })}
     </FlexRow>
   );
 }
